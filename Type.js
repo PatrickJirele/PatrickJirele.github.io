a291e240f725c3077e5c72d1df16b518c3cf641d
Patrick Jirele <pjjirele@gmail.com>
2024-04-17 12:05:15 -0600

    added type.js

---
+++ Type.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Define options for the Typed.js plugin
+    var options = {
+      strings: ['Data Scientist', 'Programmer', 'Student Athlete', 'Husband'], // Array of strings to display
+      typeSpeed: 50, // Typing speed in milliseconds
+      loop: true // Whether to loop through the strings indefinitely
+    };
+
+    // Initialize Typed.js with the provided options
+    var typed = new Typed('.typed', options);
+  });
