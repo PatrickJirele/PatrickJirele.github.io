a3014ec76170eac46014b0b93db7abfabc57c2ec
Patrick Jirele <pjjirele@gmail.com>
2024-06-22 11:48:29 -0600

    added resume

--- Type.js
+++ Type.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Define options for the Typed.js plugin
     var options = {
-      strings: ['Data Scientist', 'Programmer', 'Student Athlete', 'Husband'], // Array of strings to display
+      strings: ['Software Engineer', 'Programmer', 'Competitor', 'Husband', 'Runner'], // Array of strings to display
       typeSpeed: 85, // Typing speed in milliseconds
       loop: true // Whether to loop through the strings indefinitely
     };
