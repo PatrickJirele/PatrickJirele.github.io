0e039c6513ab8b2fd7205b39e7282caeba62335e
Patrick Jirele <pjjirele@gmail.com>
2024-06-23 13:23:12 -0600

    redesign

--- Type.js
+++ Type.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Define options for the Typed.js plugin
-    var options = {
-      strings: ['Software Engineer', 'Programmer', 'Competitor', 'Husband', 'Runner'], // Array of strings to display
-      typeSpeed: 85, // Typing speed in milliseconds
-      loop: true // Whether to loop through the strings indefinitely
-    };
+  // Define options for the Typed.js plugin
+  var options = {
+    strings: ['Software Engineer', 'Programmer', 'Competitor', 'Husband', 'Runner'], // Array of strings to display
+    typeSpeed: 85, // Typing speed in milliseconds
+    loop: true // Whether to loop through the strings indefinitely
+  };
 
-    // Initialize Typed.js with the provided options
-    var typed = new Typed('.typed', options);
-  });
+  // Initialize Typed.js with the provided options
+  var typed = new Typed('.typed', options);
+});
